8a834411c1c02953709a55e1a77c5eb5a9d70986
NicolòMaistri <nikemai115@gmail.com>
2025-07-14 17:05:15 +0200

    Small fixes 8: partial corrected

--- include/tudat/astro/aerodynamics/aerodynamicAcceleration.h
+++ include/tudat/astro/aerodynamics/aerodynamicAcceleration.h
@@ -114,13 +114,15 @@ public:
     void scaleAerodynamicAcceleration( )
     {
        currentAcceleration_ = currentUnscaledAcceleration_;
+
+       currentUnscaledAccelerationInAerodynamicFrame_ = flightConditions_->getAerodynamicAngleCalculator( )->getRotationQuaternionBetweenFrames(
+                reference_frames::inertial_frame, aerodynamicCompleteCoefficientFrame_ ) * currentAcceleration_;
+                
        if( isScalingModelSet_ )
        {
-            currentAccelerationInAerodynamicFrame_ = flightConditions_->getAerodynamicAngleCalculator( )->getRotationQuaternionBetweenFrames(
-                reference_frames::inertial_frame, aerodynamicCompleteCoefficientFrame_ ) * currentAcceleration_;
-            currentAccelerationInAerodynamicFrame_( 0 ) *= dragComponentScaling_;
-            currentAccelerationInAerodynamicFrame_( 1 ) *= sideComponentScaling_;
-            currentAccelerationInAerodynamicFrame_( 2 ) *= liftComponentScaling_;
+            currentAccelerationInAerodynamicFrame_( 0 ) = currentUnscaledAccelerationInAerodynamicFrame_( 0 ) * dragComponentScaling_;
+            currentAccelerationInAerodynamicFrame_( 1 ) = currentUnscaledAccelerationInAerodynamicFrame_( 1 ) * sideComponentScaling_;
+            currentAccelerationInAerodynamicFrame_( 2 ) = currentUnscaledAccelerationInAerodynamicFrame_( 2 ) * liftComponentScaling_;
             currentAcceleration_ = flightConditions_->getAerodynamicAngleCalculator( )->getRotationQuaternionBetweenFrames(
                 aerodynamicCompleteCoefficientFrame_, reference_frames::inertial_frame ) * currentAccelerationInAerodynamicFrame_;
        }
@@ -187,6 +189,11 @@ public:
         return currentUnscaledAcceleration_;
     }
 
+    Eigen::Vector3d getCurrentUnscaledAccelerationInAerodynamicFrame( )
+    {
+        return currentUnscaledAccelerationInAerodynamicFrame_;
+    }
+
 
 private:
 
@@ -206,6 +213,8 @@ private:
 
     // new acceleration scaling
     Eigen::Vector3d currentUnscaledAcceleration_;
+
+    Eigen::Vector3d currentUnscaledAccelerationInAerodynamicFrame_;
 
     Eigen::Vector3d currentAccelerationInAerodynamicFrame_;
 
